4775d2c8144d60d2fc601f8cc6c0bdaea4bfb34a
Anandu Mohan <34809294+anandu467@users.noreply.github.com>
2020-04-12 23:04:19 +0530

    Add files via upload

---
+++ scan.py
@@ -0,0 +1,75 @@
+
+from pyimagesearch.transform import four_point_transform
+from skimage.filters import threshold_local
+import numpy as np
+import argparse
+import cv2
+import imutils
+import scipy.misc
+from PIL import Image
+
+
+
+import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True,
+	help = "Path to the image to be scanned")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+ratio = image.shape[0] / 500.0
+orig = image.copy()
+image = imutils.resize(image, height = 500)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(gray, 75, 200)
+
+
+print("STEP 1: Edge Detection")
+
+
+
+
+
+
+# find the contours in the edged image, keeping only the
+# largest ones, and initialize the screen contour
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+
+# loop over the contours
+for c in cnts:
+	# approximate the contour
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+	# if our approximated contour has four points, then we
+	# can assume that we have found our screen
+	if len(approx) == 4:
+		screenCnt = approx
+		break
+
+# show the contour (outline) of the piece of paper
+print("STEP 2: Find contours of paper")
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+
+# apply the four point transform to obtain a top-down
+# view of the original image
+warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+
+# convert the warped image to grayscale, then threshold it
+# to give it that 'black and white' paper effect
+warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+T = threshold_local(warped, 11, offset = 10, method = "gaussian")
+warped = (warped > T).astype("uint8") * 255
+
+# show the original and scanned images
+print("STEP 3: Apply perspective transform")
+cv2.imshow("Original", imutils.resize(orig, height = 650))
+cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+cv2.waitKey(0)
+im1 = Image.fromarray(warped)
+im1.save("your_file.jpeg")
